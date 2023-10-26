71154ee98979f68281b8f055ad0915ae5488ffbc
philipwanjiku <muhorophilip7@gmail.com>
2023-10-26 09:11:04 +0300

    HTML elements using selectors

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,15 @@
 //definitions
 let curCity= "Nairobi";
 let units= "metric";
+
+//Get HTML elements using selectors
+let city= document.querySelector(".weather_city");
+let datetime= document.querySelector(".weather_datetime");
+let weather_forecast= document.querySelector(".weather_forecast");
+let weather_temperature= document.querySelector(".weather_temperature");
+let weather_icon= document.querySelector(".weather_icon");
+let weather_minmax= document.querySelector(".weather_minmax");
+let weather_realfeel= document.querySelector(".weather_realfeel");
+let weather_humidity= document.querySelector(".weather_humidity");
+let weather_wind= document.querySelector(".weather_wind");
+let weather_pressure= document.querySelector(".weather_pressure");
